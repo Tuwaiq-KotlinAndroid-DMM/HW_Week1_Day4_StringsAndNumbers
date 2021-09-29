bd646661fbd12dcb4789fdb70c8d6e36749db5b3
AbdulazizAlrajeh <Abdulaziz.Alrajeh.dmm.k1@tuwaiq.edu.sa>
2021-09-29 18:10:52 +0300

    This my home work for part 2 (Abdulaziz Alrajeh).

---
+++ homework/answerPart2/answerQuestion1/src/main/kotlin/Main.kt
@@ -0,0 +1,13 @@
+fun main(args: Array<String>) {
+    // What program doing
+
+    // Declare two variables
+   var old = "The quick brown fox jumps over the lazy dog."
+
+    // substring from variable old
+    var new = old.substring(10,25)
+
+    // print new string
+    println(new)
+
+}
