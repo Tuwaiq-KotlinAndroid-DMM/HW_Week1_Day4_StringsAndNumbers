2f97a73087b0726747bd14da9349f86bd8dc9550
alinabil4905 <alinabil4905@gmail.com>
2021-09-29 18:17:50 +0300

    HW4

---
+++ src/main/kotlin/Q2.kt
@@ -0,0 +1,9 @@
+fun main(args: Array<String>) {
+
+
+var old = "The quick brown fox jumps over the lazy dog."
+
+    var new = old.substring(10..24)
+
+    println(new)
+}
---
+++ src/main/kotlin/Q3.kt
@@ -0,0 +1,10 @@
+fun main(args: Array<String>) {
+
+
+    var F = readLine() !!.toInt()
+    var c = (5*(F-32))/ 9
+
+    println("temperature in C is :$c")
+
+
+}
